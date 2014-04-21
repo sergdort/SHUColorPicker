f5cbac3a88f6525556ab3f61c5d61174244307e8
Sergey Shulga <sergey.shulga@softheme.com>
2014-04-21 16:31:01 +0300

    added example work space

---
+++ Example/SHUColorPickerExample/SHUViewController.m
@@ -0,0 +1,29 @@
+//
+//  SHUViewController.m
+//  SHUColorPickerExample
+//
+//  Created by Sergey Shulga on 4/21/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "SHUViewController.h"
+
+@interface SHUViewController ()
+
+@end
+
+@implementation SHUViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
